198725e6e03b74b2b7a3281f9b51e78e53ec086f
Lacrizomiq <lufo.rn84@gmail.com>
2024-04-03 20:29:49 +0200

    ui

--- src/pages/pricing/MassageSportif.tsx
+++ src/pages/pricing/MassageSportif.tsx
@@ -3,7 +3,7 @@ import { IoMdCheckmark } from "react-icons/io";
 
 const MassageSportif = () => {
   return (
-    <div className="flex flex-col min-w-[300px] min-h-[300px] max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-4">
+    <div className="flex flex-col min-w-[350px] min-h-[300px] max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-4">
       <div>
         <div className="font-bold text-xl mb-2 flex justify-center items-center">
           <TbMassage /> <p className="ml-4">Massage sportif</p>
@@ -29,16 +29,18 @@ const MassageSportif = () => {
           </ul>
         </p>
       </div>
-      <div className="mt-auto pt-4 pb-2">
+      <div className="flex mt-auto pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           60 minutes
         </span>
-        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700">
+        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 mr-2 mb-2">
           60 €
         </span>
-        <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 ml-8">
-          En savoir plus !
-        </span>
+        <div>
+          <span className="inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-blue-700 ml-4">
+            En savoir plus !
+          </span>
+        </div>
       </div>
     </div>
   );
